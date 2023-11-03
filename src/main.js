40ef41021c4950edbc057344fc1a7b1345568e97
Edit-Mr <elvismao.070512@gmail.com>
2023-11-03 12:51:50 +0800

    add lcd test

--- src/main.js
+++ src/main.js
@@ -9,3 +9,21 @@ fetch('https://raw.githubusercontent.com/SCAICT/website-data/main/home.json')
     .catch(error => {
         console.error('Error:', error);
     });
+
+const header = document.querySelector('header');
+
+header.addEventListener('mousemove', (e) => {
+    const x = e.clientX / window.innerWidth * 10 - 5;
+    const y = e.clientY / window.innerHeight * 10 - 5;
+
+    header.style.transform = `translate(${-x}vh, ${-y}vh)`;
+})
+
+//Use gyroscopes on mobile devices
+
+window.addEventListener('deviceorientation', (e) => {
+    const x = e.gamma / 10;
+    const y = e.beta / 10;
+document.querySelector('.lcd').innerHTML = x;
+    header.style.transform = `translate(${-x}vh, ${-y}vh)`;
+})
