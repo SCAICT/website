fa9374c30f2afffafe2e5d3dc186e7087e23e162
Edit-Mr <elvismao.070512@gmail.com>
2023-11-07 11:27:54 +0800

    save

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ function permission() {
                 window.addEventListener("deviceorientation", (e) => {
                     x = (rotation ? e.beta : e.gamma) / 6;
                     y = (rotation ? -e.gamma : e.beta) / 6;
-                    header.style.transform = `translate(${-x}vh, ${-y}vh)`;
+                    header.style.transform = `translate3d(${-x}vh, ${-y}vh,0)`;
                 })
             }
         })
@@ -51,14 +51,21 @@ window.addEventListener('resize', () => {
 // Scale big header on scroll
 
 const bigHeader = document.querySelector('header');
-const About = document.querySelector('#About');
+const container = document.querySelector('.container');
 
-window.addEventListener('scroll', () => {
-    bigHeader.style.transform = `scale(${1 + window.scrollY / window.innerHeight*80})`;
-    if(window.scrollY > window.innerHeight*0.1) {
-        bigHeader.style.opacity = 1-window.scrollY / window.innerHeight*3;
-        About.style.opacity = window.scrollY / window.innerHeight*3;
-    }else {
+window.addEventListener('scroll', (e) => {
+    if (window.scrollY < window.innerHeight * 1.15 / 3) bigHeader.style.transform = `scale(${1 + window.scrollY / window.innerHeight * 80})`;
+    if (window.scrollY > window.innerHeight * 0.05) {
+        console.log(About.style.opacity)
+        if (window.scrollY < window.innerHeight * 1.15 / 3) {
+            bigHeader.style.opacity = 1 - (window.scrollY - window.innerHeight * 0.05) / window.innerHeight * 3;
+            About.style.opacity = (window.scrollY - window.innerHeight * 0.05) / window.innerHeight * 3;
+        } else {
+            bigHeader.style.opacity = 0;
+            About.style.opacity = 1;
+            container.scrollLeft += e.deltaY;
+        }
+    } else {
         bigHeader.style.opacity = 1;
         About.style.opacity = 0;
     }
