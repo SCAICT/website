52c3dc58602e59c55378908a987767b079c742ad
Edit-Mr <elvismao.070512@gmail.com>
2023-11-08 08:48:23 +0800

    finished to activity

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # website
-中電會官方網站
+
+中電會官方網站靜態前端代碼
+
+![Workflow](https://img.shields.io/github/deployments/SCAICT/Website/production?label=deployment&style=flat-square) ![code style](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)
+
+## 格式化
+
+請使用 [Prettier](https://prettier.io/) 格式化代碼。
+
+```json
+{
+    "arrowParens": "avoid",
+    "bracketSpacing": true,
+    "endOfLine": "lf",
+    "htmlWhitespaceSensitivity": "css",
+    "insertPragma": true,
+    "singleAttributePerLine": false,
+    "bracketSameLine": false,
+    "jsxBracketSameLine": false,
+    "jsxSingleQuote": false,
+    "printWidth": 80,
+    "proseWrap": "preserve",
+    "quoteProps": "as-needed",
+    "requirePragma": false,
+    "semi": true,
+    "singleQuote": false,
+    "tabWidth": 4,
+    "trailingComma": "es5",
+    "useTabs": false,
+    "embeddedLanguageFormatting": "auto",
+    "vueIndentScriptAndStyle": false,
+    "parser": "babel"
+  }
+```
--- src/main.js
+++ src/main.js
@@ -1,29 +1,36 @@
-fetch('https://raw.githubusercontent.com/SCAICT/website-data/main/home.json')
+/** @format */
+
+fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/home.json")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        document.querySelector('.lcd').innerHTML = data.lcd.text;
-        document.getElementById('lcdLink').href = data.lcd.link;
-        document.getElementById('lcdLink').target = '_blank';
+        document.querySelector(".lcd").innerHTML = data.lcd.text;
+        document.getElementById("lcdLink").href = data.lcd.link;
+        document.getElementById("lcdLink").target = "_blank";
     })
     .catch(error => {
-        console.error('Error:', error);
+        console.error("Error:", error);
     });
 
-
 // Mouse Move Effect
-const header = document.querySelector('header');
-header.addEventListener('mousemove', (e) => {
-    const x = e.clientX / window.innerWidth * 10 - 5;
-    const y = e.clientY / window.innerHeight * 10 - 5;
+const header = document.querySelector("header");
+header.addEventListener("mousemove", e => {
+    const x = (e.clientX / window.innerWidth) * 10 - 5;
+    const y = (e.clientY / window.innerHeight) * 10 - 5;
     header.style.transform = `translate(${-x}vh, ${-y}vh)`;
-})
+    bigHeader.style.opacity = 1;
+    About.style.opacity = 0;
+});
 
 // Device Orientation Effect
-var phone = (/Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-var rotation = false, x, y;
+var phone = /Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+);
+var rotation = false,
+    x,
+    y;
 function checkRotation() {
-    rotation = (window.innerHeight < window.innerWidth && phone);
+    rotation = window.innerHeight < window.innerWidth && phone;
 }
 
 function permission() {
@@ -31,42 +38,68 @@ function permission() {
     DeviceOrientationEvent.requestPermission()
         .then(response => {
             if (response == "granted") {
-                window.addEventListener("deviceorientation", (e) => {
+                window.addEventListener("deviceorientation", e => {
                     x = (rotation ? e.beta : e.gamma) / 6;
                     y = (rotation ? -e.gamma : e.beta) / 6;
                     header.style.transform = `translate3d(${-x}vh, ${-y}vh,0)`;
-                })
+                });
             }
         })
-        .catch(console.error)
+        .catch(console.error);
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     checkRotation();
-    permission()
-})
+    permission();
+});
 
 //Scroll Effect
 
 // Scale big header on scroll
 
-const bigHeader = document.querySelector('header');
-const container = document.querySelector('.container');
+const bigHeader = document.querySelector("header");
+const container = document.querySelector(".container");
+const PastActivities = document.querySelector("#PastActivities");
+var last;
+window.addEventListener("scroll", () => {
+    // 放大效果
+    if (window.scrollY < (window.innerHeight * 1.15) / 3)
+        bigHeader.style.transform = `scale(${
+            1 + (window.scrollY / window.innerHeight) * 80
+        })`;
 
-window.addEventListener('scroll', (e) => {
-    if (window.scrollY < window.innerHeight * 1.15 / 3) bigHeader.style.transform = `scale(${1 + window.scrollY / window.innerHeight * 80})`;
+    // 透明度轉場
     if (window.scrollY > window.innerHeight * 0.05) {
-        console.log(About.style.opacity)
-        if (window.scrollY < window.innerHeight * 1.15 / 3) {
-            bigHeader.style.opacity = 1 - (window.scrollY - window.innerHeight * 0.05) / window.innerHeight * 3;
-            About.style.opacity = (window.scrollY - window.innerHeight * 0.05) / window.innerHeight * 3;
+        if (window.scrollY < (window.innerHeight * 1.15) / 3) {
+            bigHeader.style.pointerEvents = "all";
+            bigHeader.style.opacity =
+                1 -
+                ((window.scrollY - window.innerHeight * 0.05) /
+                    window.innerHeight) *
+                    3;
+            About.style.opacity =
+                ((window.scrollY - window.innerHeight * 0.05) /
+                    window.innerHeight) *
+                3;
         } else {
+            // 轉場結束
+            bigHeader.style.pointerEvents = "none";
             bigHeader.style.opacity = 0;
             About.style.opacity = 1;
-            container.scrollLeft += e.deltaY;
+            // Get the right offset of #PastActivities to the screen
+            console.log(  PastActivities.getBoundingClientRect().left )
+            // if (
+            //     PastActivities.getBoundingClientRect().left >=
+            //     0
+            // )
+                container.style.transform =
+                    "translateX(" +
+                    ((window.innerHeight * 1.15) / 3 - window.scrollY) +
+                    "px)";
+                    
         }
     } else {
         bigHeader.style.opacity = 1;
         About.style.opacity = 0;
     }
-})
+});
