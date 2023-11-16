fd6f8e77ed3281b4bc0189372e4473643a34f749
GitHub Action <action@github.com>
2023-11-16 01:14:15 +0000

    Minify Code

--- src/main.js
+++ src/main.js
@@ -1,169 +1,21 @@
-/** @format */
-
-fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/home.json")
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector(".lcd").innerHTML = data.lcd.text;
-        document.getElementById("lcdLink").href = data.lcd.link;
-        document.getElementById("lcdLink").target = "_blank";
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
-
-// Events
-fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/events.json")
-    .then(response => response.json())
-    .then(data => {
-        var events = data.activities;
-        var eventsHTML = "";
-        for (var i = 0; i < events.length; i++) {
-            eventsHTML += `
+fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/home.json").then(a=>a.json()).then(a=>{document.querySelector(".lcd").innerHTML=a.lcd.text,document.getElementById("lcdLink").href=a.lcd.link,document.getElementById("lcdLink").target="_blank"}).catch(a=>{console.error("Error:",a)}),fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/events.json").then(a=>a.json()).then(a=>{for(var b=a.activities,c="",d=0;d<b.length;d++)c+=`
             <article>
-            ${
-                events[i].image != ""
-                    ? '<img src="' + events[i].image + '" />'
-                    : ""
-            }
+            ${""==b[d].image?"":"<img src=\""+b[d].image+"\" />"}
                 <div>
-                    <h4>${events[i].title}</h4>
-                    <h5>${events[i].description}</h5>
+                    <h4>${b[d].title}</h4>
+                    <h5>${b[d].description}</h5>
                     <ul>
                         <li>
                             <i class="fa-solid fa-calendar"></i>
-                            ${events[i].date}
+                            ${b[d].date}
                         </li>
                         <li>
                             <i class="fa-solid fa-location-dot"></i>
-                            ${events[i].location}
+                            ${b[d].location}
                         </li>
                         <li>
-                            <i class="fa-solid fa-tag"></i> ${events[i].price}
+                            <i class="fa-solid fa-tag"></i> ${b[d].price}
                         </li>
                     </ul>
                 </div>
-            </article>`;
-        }
-        document.querySelector(".events").innerHTML = eventsHTML;
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
-
-// Mouse Move Effect
-const header = document.querySelector("header");
-header.addEventListener("mousemove", e => {
-    const x = (e.clientX / window.innerWidth) * 10 - 5;
-    const y = (e.clientY / window.innerHeight) * 10 - 5;
-    header.style.transform = `translate(${-x}vh, ${-y}vh)`;
-    bigHeader.style.opacity = 1;
-    About.style.opacity = 0;
-});
-
-// Device Orientation Effect
-var phone = /Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-);
-var rotation = false,
-    x,
-    y;
-function checkRotation() {
-    rotation = window.innerHeight < window.innerWidth && phone;
-}
-
-function permission() {
-    if (!DeviceOrientationEvent.requestPermission) return;
-    DeviceOrientationEvent.requestPermission()
-        .then(response => {
-            if (response == "granted") {
-                window.addEventListener("deviceorientation", e => {
-                    x = (rotation ? e.beta : e.gamma) / 6;
-                    y = (rotation ? -e.gamma : e.beta) / 6;
-                    header.style.transform = `translate3d(${-x}vh, ${-y}vh,0)`;
-                });
-            }
-        })
-        .catch(console.error);
-}
-
-window.addEventListener("resize", () => {
-    checkRotation();
-    permission();
-});
-
-//Scroll Effect
-
-// Scale big header on scroll
-
-const bigHeader = document.querySelector("header");
-const container = document.querySelector(".container");
-const PastActivities = document.querySelector("#Pastevents");
-
-let containerScrollWidth;
-window.onload = () => {
-    containerScrollWidth =
-        container.offsetWidth -
-        window.innerWidth +
-        (window.innerHeight * 1.15) / 3;
-};
-// for scroll don't need original window.innerHeight;
-
-var last = window.scrollY;
-window.addEventListener("scroll", () => {
-    // 放大效果
-    if (window.scrollY < (window.innerHeight * 1.15) / 3)
-        bigHeader.style.transform = `scale(${
-            1 + (window.scrollY / window.innerHeight) * 80
-        })`;
-    // 透明度轉場
-    if (window.scrollY > window.innerHeight * 0.05) {
-        if (window.scrollY < (window.innerHeight * 1.15) / 3) {
-            bigHeader.style.pointerEvents = "all";
-            bigHeader.style.opacity =
-                1 -
-                ((window.scrollY - window.innerHeight * 0.05) /
-                    window.innerHeight) *
-                    3;
-            About.style.opacity =
-                ((window.scrollY - window.innerHeight * 0.05) /
-                    window.innerHeight) *
-                3;
-        } else {
-            // 轉場結束
-            bigHeader.style.pointerEvents = "none";
-            bigHeader.style.opacity = 0;
-            About.style.opacity = 1;
-            if (window.scrollY < containerScrollWidth)
-                container.style.transform =
-                    "translateX(" +
-                    ((window.innerHeight * 1.15) / 3 - window.scrollY) +
-                    "px)";
-            else
-                container.style.transform =
-                    "translate(-" +
-                    (container.offsetWidth - window.innerWidth) +
-                    "px," +
-                    ((window.innerHeight * 1.15) / 3 -
-                        window.scrollY +
-                        containerScrollWidth -
-                        window.innerHeight / 2) +
-                    "px)";
-            // last = window.scrollY;
-            // else
-            //     container.style.transform =
-            //         "translateX(" +
-            //         ((window.innerHeight * 1.15) / 3 -
-            //             window.scrollY -
-            //             (Pastevents.getBoundingClientRect().left -
-            //                 (window.scrollY - last))) +
-            //         "px)";
-        }
-    } else {
-        bigHeader.style.opacity = 1;
-        About.style.opacity = 0;
-    }
-});
-
-// // get the width of container
-// document.body.style.height =
-//     (window.innerHeight * 1.15) / 3 + container.offsetWidth + "px";
+            </article>`;document.querySelector(".events").innerHTML=c}).catch(a=>{console.error("Error:",a)});const header=document.querySelector("header");header.addEventListener("mousemove",a=>{const b=10*(a.clientX/window.innerWidth)-5,c=10*(a.clientY/window.innerHeight)-5;header.style.transform=`translate(${-b}vh, ${-c}vh)`,bigHeader.style.opacity=1,About.style.opacity=0});var x,y,phone=/Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),rotation=!1;function checkRotation(){rotation=window.innerHeight<window.innerWidth&&phone}function permission(){DeviceOrientationEvent.requestPermission&&DeviceOrientationEvent.requestPermission().then(a=>{"granted"==a&&window.addEventListener("deviceorientation",a=>{x=(rotation?a.beta:a.gamma)/6,y=(rotation?-a.gamma:a.beta)/6,header.style.transform=`translate3d(${-x}vh, ${-y}vh,0)`})}).catch(console.error)}window.addEventListener("resize",()=>{checkRotation(),permission()});const bigHeader=document.querySelector("header"),container=document.querySelector(".container"),PastActivities=document.querySelector("#Pastevents");let containerScrollWidth;window.onload=()=>{containerScrollWidth=container.offsetWidth-window.innerWidth+1.15*window.innerHeight/3};var last=window.scrollY;window.addEventListener("scroll",()=>{window.scrollY<1.15*window.innerHeight/3&&(bigHeader.style.transform=`scale(${1+80*(window.scrollY/window.innerHeight)})`),window.scrollY>.05*window.innerHeight?window.scrollY<1.15*window.innerHeight/3?(bigHeader.style.pointerEvents="all",bigHeader.style.opacity=1-3*((window.scrollY-.05*window.innerHeight)/window.innerHeight),About.style.opacity=3*((window.scrollY-.05*window.innerHeight)/window.innerHeight)):(bigHeader.style.pointerEvents="none",bigHeader.style.opacity=0,About.style.opacity=1,container.style.transform=window.scrollY<containerScrollWidth?"translateX("+(1.15*window.innerHeight/3-window.scrollY)+"px)":"translate(-"+(container.offsetWidth-window.innerWidth)+"px,"+(1.15*window.innerHeight/3-window.scrollY+containerScrollWidth-window.innerHeight/2)+"px)"):(bigHeader.style.opacity=1,About.style.opacity=0)});
