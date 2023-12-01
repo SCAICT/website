d0c01b08420d5b44b9a7f6ff5c3b521d622f9ee6
Edit-Mr <elvismao.070512@gmail.com>
2023-12-01 15:10:41 +0800

    New design for img + biob image

--- src/main.js
+++ src/main.js
@@ -1,97 +1,111 @@
 /** @format */
 
-fetch('https://raw.githubusercontent.com/SCAICT/website-data/main/home.json')
+fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/home.json")
     .then(response => response.json())
     .then(data => {
-        document.querySelector('.lcd').innerHTML = data.lcd.text;
-        document.getElementById('lcdLink').href = data.lcd.link;
-        document.getElementById('lcdLink').target = '_blank';
+        document.querySelector(".lcd").innerHTML = data.lcd.text;
+        document.getElementById("lcdLink").href = data.lcd.link;
+        document.getElementById("lcdLink").target = "_blank";
     })
     .catch(error => {
-        console.error('Error: ', error);
+        console.error("Error: ", error);
     });
 
 // Events
-fetch('https://raw.githubusercontent.com/SCAICT/website-data/main/events.json')
+fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/events.json")
     .then(response => response.json())
     .then(data => {
         const events = data.activities;
-        let eventsHTML = '';
+        let eventsHTML = "";
         for (const event of events) {
             eventsHTML += `
             <article>${
-                event.image != ''
-                    ? `<img src="${event.image}"/>`
-                    : ''
-            }<div><h4>${event.title}</h4><h5>${
-                event.description
-            }</h5><ul><li><i class="fa-solid fa-calendar"></i> ${
+                event.image != "" ? `<img src="${event.image}"/>` : ""
+            }<div><h4>${
+                event.title
+            }</h4><ul><li><i class="fa-solid fa-calendar"></i> ${
                 event.date
             }</li><li><i class="fa-solid fa-location-dot"></i> ${
                 event.location
             }</li><li><i class="fa-solid fa-tag"></i> ${
                 event.price
-            }</li></ul></div></article>`;
+            }</li></ul><h5>${event.description}</h5></div></article>`;
         }
-        document.querySelector('.events').innerHTML = eventsHTML;
+        document.querySelector(".events").innerHTML = eventsHTML;
     })
     .catch(error => {
-        console.error('Error:', error);
+        console.error("Error:", error);
     });
 
 // Images
 let imageData,
     pressed = false,
     imageIndex = 0;
-fetch('https://raw.githubusercontent.com/SCAICT/website-data/main/images.json')
+fetch("https://raw.githubusercontent.com/SCAICT/website-data/main/images.json")
     .then(response => response.json())
     .then(data => {
         imageData = data;
-        let sdBox = document.querySelector('.sdBox');
+        console.log(imageData);
+        let sdBox = document.querySelector(".sdBox");
         for (const event in imageData) {
-            const div = document.createElement('div');
-            div.classList.add('sdCard');
+            const div = document.createElement("div");
+            div.classList.add("sdCard");
             div.textContent = event;
             sdBox.appendChild(div);
-            div.addEventListener('click', showImg);
+            div.addEventListener("click", showImg);
         }
     })
     .catch(error => {
-        console.error('Error: ', error);
+        console.error("Error: ", error);
     });
 
-const sd = document.querySelector('.sd');
-const imgContainer = document.getElementById('showImg-container');
+const sd = document.querySelector(".sd");
+const imgContainer = document.getElementById("showImg-container");
 const showImg = event => {
     if (pressed)
-        document.querySelector('.selected-sd').classList.remove('selected-sd');
+        document.querySelector(".selected-sd").classList.remove("selected-sd");
     else pressed = true;
     const activityName = event.target.innerText;
-    event.target.classList.add('selected-sd');
-    sd.style.left = '-25vh';
+    event.target.classList.add("selected-sd");
+    sd.style.left = "-25vh";
     imgContainer.innerHTML = '<div class="showImg"></div>';
     imageIndex = 0;
     setTimeout(() => {
         sd.style.left = `calc(20vh / var(--h))`;
         setTimeout(() => {
-            imgContainer.innerHTML = '';
-            for (const i = 0; i < imageData[activityName].length; i++) {
-                const div = document.createElement('div');
-                div.classList.add('showImg');
-                div.style.backgroundImage = `url(https://raw.githubusercontent.com/SCAICT/website-data/main/converted/img/${activityName}/${imageData[activityName][i]})`;
+            imgContainer.innerHTML = "";
+            for (var i = 0; i < imageData[activityName].length; i++) {
+                const div = document.createElement("div");
+                div.classList.add("showImg");
+                var imgLink = `https://raw.githubusercontent.com/SCAICT/website-data/main/converted/img/${activityName}/${imageData[activityName][i]}`;
+
+                // Fetch image and convert to base64
+                fetch(imgLink)
+                    .then(response => response.blob())
+                    .then(blob => {
+                        const reader = new FileReader();
+                        reader.onloadend = () => {
+                            div.style.backgroundImage = `url(${reader.result})`;
+                        };
+                        reader.readAsDataURL(blob);
+                    })
+                    .catch(error => {
+                        console.error("Error fetching image:", error);
+                    });
+
                 imgContainer.appendChild(div);
             }
             document
-                .querySelector('#showImg-container .showImg:last-child')
-                .classList.add('displaying');
+                .querySelector("#showImg-container .showImg:last-child")
+                .classList.add("displaying");
         }, 500);
     }, 500);
     sd.innerText = activityName;
 };
 const changeImg = (direction = 1) => {
     document
-        .querySelector('#showImg-container .showImg.displaying')
-        .classList.remove('displaying');
+        .querySelector("#showImg-container .showImg.displaying")
+        .classList.remove("displaying");
     imageIndex += direction;
     if (imageIndex < 0) imageIndex = imageData[sd.innerText].length - 1;
     if (imageIndex >= imageData[sd.innerText].length) imageIndex = 0;
@@ -99,12 +113,12 @@ const changeImg = (direction = 1) => {
         .querySelector(
             `#showImg-container .showImg:nth-child(${imageIndex + 1})`
         )
-        .classList.add('displaying');
+        .classList.add("displaying");
 };
 
 // Mouse Move Effect
-const header = document.querySelector('header');
-header.addEventListener('mousemove', e => {
+const header = document.querySelector("header");
+header.addEventListener("mousemove", e => {
     const x = (e.clientX / window.innerWidth) * 10 - 5;
     const y = (e.clientY / window.innerHeight) * 10 - 5;
     header.style.transform = `translate(${-x}vh, ${-y}vh)`;
@@ -127,25 +141,25 @@ function checkRotation() {
         window.innerWidth +
         (window.innerHeight * 1.15) / 3;
     SponsorBoxForHeight = document.querySelector(
-        '.SponsorBoxForHeight'
+        ".SponsorBoxForHeight"
     ).offsetHeight;
     document.body.style.height =
         containerScrollWidth +
         SponsorBoxForHeight -
         parseFloat(getComputedStyle(document.documentElement).fontSize) * 3.5 +
-        'px';
-    document.getElementById('About_').style.height =
-        document.querySelector('.before-goals').offsetWidth + 'px';
-    document.getElementById('Project_').style.height =
-        document.querySelector('#Project').offsetWidth + 'px';
+        "px";
+    document.getElementById("About_").style.height =
+        document.querySelector(".before-goals").offsetWidth + "px";
+    document.getElementById("Project_").style.height =
+        document.querySelector("#Project").offsetWidth + "px";
 }
 
 function permission() {
     if (!DeviceOrientationEvent.requestPermission) return;
     DeviceOrientationEvent.requestPermission()
         .then(response => {
-            if (response == 'granted') {
-                window.addEventListener('deviceorientation', e => {
+            if (response == "granted") {
+                window.addEventListener("deviceorientation", e => {
                     x = (rotation ? e.beta : e.gamma) / 6;
                     y = (rotation ? -e.gamma : e.beta) / 6;
                     header.style.transform = `translate3d(${-x}vh, ${-y}vh,0)`;
@@ -155,7 +169,7 @@ function permission() {
         .catch(console.error);
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     checkRotation();
     permission();
 });
@@ -164,13 +178,13 @@ window.addEventListener('resize', () => {
 
 // Scale big header on scroll
 
-const bigHeader = document.querySelector('header');
-const container = document.querySelector('.container');
-const PastActivities = document.querySelector('#Pastevents');
-const charger = document.querySelector('.sponsor-fixed .sponsor-charger');
-const fixedCharger = document.querySelector('.sponsor-charger.fixed');
-const clubTitle = document.getElementById('clubTitle');
-const introImg = document.querySelector('.introImg');
+const bigHeader = document.querySelector("header");
+const container = document.querySelector(".container");
+const PastActivities = document.querySelector("#Pastevents");
+const charger = document.querySelector(".sponsor-fixed .sponsor-charger");
+const fixedCharger = document.querySelector(".sponsor-charger.fixed");
+const clubTitle = document.getElementById("clubTitle");
+const introImg = document.querySelector(".introImg");
 let containerScrollWidth, SponsorBoxForHeight;
 window.onload = () => {
     checkRotation();
@@ -184,7 +198,7 @@ const scrollFunction = () => {
     // 透明度轉場
     if (window.scrollY > window.innerHeight * 0.05) {
         if (window.scrollY < (window.innerHeight * 1.15) / 3) {
-            bigHeader.style.pointerEvents = 'all';
+            bigHeader.style.pointerEvents = "all";
             bigHeader.style.opacity =
                 1 -
                 ((window.scrollY - window.innerHeight * 0.05) /
@@ -196,16 +210,16 @@ const scrollFunction = () => {
                 3;
         } else {
             // 轉場結束
-            bigHeader.style.pointerEvents = 'none';
+            bigHeader.style.pointerEvents = "none";
             bigHeader.style.opacity = 0;
             About.style.opacity = 1;
             if (window.scrollY < containerScrollWidth) {
                 container.style.transform =
-                    'translateX(' +
+                    "translateX(" +
                     ((window.innerHeight * 1.15) / 3 - window.scrollY) +
-                    'px)';
+                    "px)";
                 const projectOffset = document
-                    .getElementById('Project')
+                    .getElementById("Project")
                     .getBoundingClientRect().left;
                 // 0.684
                 if (
@@ -218,36 +232,36 @@ const scrollFunction = () => {
                                 (window.innerWidth +
                                     window.innerHeight * 0.684)) *
                             100 +
-                        '%';
+                        "%";
                     console.log(
-                        (1 - projectOffset / window.innerWidth) * 100 + '%'
+                        (1 - projectOffset / window.innerWidth) * 100 + "%"
                     );
                 }
             } else {
                 container.style.transform =
-                    'translate(-' +
+                    "translate(-" +
                     (container.offsetWidth - window.innerWidth) +
-                    'px,' +
+                    "px," +
                     ((window.innerHeight * 1.15) / 3 -
                         window.scrollY +
                         containerScrollWidth -
                         window.innerHeight / 2) +
-                    'px)';
+                    "px)";
             }
             fixedCharger.classList.toggle(
-                'rise',
+                "rise",
                 window.scrollY + window.innerHeight / 5.5 > containerScrollWidth
             );
             if (clubTitle.getBoundingClientRect().top < window.innerHeight) {
                 fixedCharger.style.transform =
-                    'translateY(' +
+                    "translateY(" +
                     (clubTitle.getBoundingClientRect().top -
                         window.innerHeight) +
-                    'px)';
-                fixedCharger.style.transitionDuration = '.1s';
+                    "px)";
+                fixedCharger.style.transitionDuration = ".1s";
             } else {
-                fixedCharger.style.transform = '';
-                fixedCharger.style.transitionDuration = '.4s';
+                fixedCharger.style.transform = "";
+                fixedCharger.style.transitionDuration = ".4s";
             }
         }
     } else {
@@ -257,15 +271,15 @@ const scrollFunction = () => {
 };
 
 const last = window.scrollY;
-window.addEventListener('scroll', scrollFunction);
+window.addEventListener("scroll", scrollFunction);
 scrollFunction();
 
-elements = document.querySelectorAll('.tree i');
+elements = document.querySelectorAll(".tree i");
 elements.forEach(element => {
-    element.addEventListener('click', () => {
-        element.style.top = '100%';
+    element.addEventListener("click", () => {
+        element.style.top = "100%";
         setTimeout(() => {
-            window.open(element.getAttribute('link'), '_blank');
+            window.open(element.getAttribute("link"), "_blank");
         }, 300);
     });
 });
